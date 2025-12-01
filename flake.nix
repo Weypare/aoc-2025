a32fa3be1f7d5d5e795f6542d8012fa6f08f1d5a
Andrii Avramenko <andreyavr@gmail.com>
2025-12-02 00:55:16 +0200

    chore: remove treefmt

--- flake.nix
+++ flake.nix
@@ -4,18 +4,13 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.devshell.flakeModule
-        inputs.treefmt-nix.flakeModule
-      ];
+      imports = [inputs.devshell.flakeModule];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
         config,
@@ -33,11 +28,7 @@
             pkgs.just
           ];
         };
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs.odinfmt.enable = true;
-          programs.alejandra.enable = true;
-        };
+        formatter = pkgs.alejandra;
       };
     };
 }
